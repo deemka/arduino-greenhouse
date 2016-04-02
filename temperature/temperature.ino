8256cb70f547287bf20fd544cfa8b0b9f05b47c4
deemka <root@ggl.com>
2016-04-02 19:35:24 +0200

    Added Servo with detach

--- temperature/temperature.ino
+++ temperature/temperature.ino
@@ -1,13 +1,19 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <Servo.h>
 
-#define TEMP_PIN 2
+#define TEMP_PIN  2
+#define SERVO_PIN 5
 
-OneWire oneWire(TEMP_PIN);
+OneWire           oneWire(TEMP_PIN);
 DallasTemperature sensors(&oneWire);
+Servo             servo; 
+
 float t_meas;
 int const t_crit[] = {20, 30, 40};
 int NUM_OF_BINS = 0;
+int mode = -1;
+int prev_mode = 0;
 
 int get_bin_index(int const ranges[], int meas)
 {
@@ -25,6 +31,8 @@ void setup(void)
     Serial.begin(9600);
     sensors.begin();
     NUM_OF_BINS = sizeof(t_crit)/sizeof(t_crit[0]) + 1;
+
+    servo.attach(SERVO_PIN);
 }
 
 void loop(void)
@@ -34,7 +42,18 @@ void loop(void)
     Serial.print("T = ");
     Serial.print(t_meas);
     Serial.print(" C (Stufe ");
-    Serial.print(get_bin_index(t_crit, int(t_meas+.5)));
+
+    prev_mode = mode;
+    mode = get_bin_index(t_crit, int(t_meas+.5));
+    Serial.print(mode);
     Serial.println(")");
+
+    if (mode != prev_mode) {
+	servo.attach(SERVO_PIN);
+	servo.write(mode*60);
+	delay(1000);
+	servo.detach();
+    }
+
     delay(1000);
 }
